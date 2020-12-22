5c7ccb4ced8c1a42d4ddc13436d315b63bf11854
Martin Treacy-Schwartz <mts@gameanalytics.com>
2020-12-22 12:39:02 +0100

    Updated to v1.2.3

--- README.md
+++ README.md
@@ -15,6 +15,9 @@ If you have any issues or feedback regarding the SDK, please contact our friendl
 Changelog
 ---------
 <!--(CHANGELOG_TOP)-->
+**1.2.3**
+* removed memory info from automatic crash reports
+
 **1.2.2**
 * fixed crash when closing editor and GA module was loaded
 
--- android_godot_3_2/godotgameanalytics/src/main/java/com/gameanalytics/godotgameanalytics/GodotGameAnalytics.java
+++ android_godot_3_2/godotgameanalytics/src/main/java/com/gameanalytics/godotgameanalytics/GodotGameAnalytics.java
@@ -18,7 +18,7 @@ import java.util.Set;
 public class GodotGameAnalytics extends GodotPlugin
 {
     private Godot activity = null;
-    private static final String VERSION = "godot 1.2.2";
+    private static final String VERSION = "godot 1.2.3";
 
     public GodotGameAnalytics(Godot godot)
     {
